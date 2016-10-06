641a6bc9063e11f19774b5a5d384ab6e59d1380c
Jovi De Croock <jovi.decroock.u8420@student.hogent.be>
2016-10-06 16:53:36 +0200

    start

--- StuckytoysBackend/routes/app.js
+++ StuckytoysBackend/routes/app.js
@@ -5,7 +5,10 @@
 
 
 // Dependencies express etc
-
+var mongoose = require('mongoose');
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
 
 //models
 var User = require('../models/Users.js');
@@ -14,10 +17,18 @@ var User = require('../models/Users.js');
 User.methods['get', 'post'];
 
 //API routes
-User.route('Drawings',
+User.route('Drawings', function(req, res, next)
     {
+        User.find(function(err, drawings)
+        {
+           if(err){return next(err);}
+           res.json(drawings);
+        });
        //zoeken hoe ik via mongo de drawings krijg :)
     });
 
 
+router.post('/register', function(req, res, next)
+{
 
+});
