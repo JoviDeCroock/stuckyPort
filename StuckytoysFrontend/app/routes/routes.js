669fb103673c80300adfd88c4d68ef72202e7376
Joren Saey <joren.saey.t2795@student.hogent.be>
2016-10-26 16:56:39 +0200

    fix

--- StuckytoysFrontend/app/routes/routes.js
+++ StuckytoysFrontend/app/routes/routes.js
@@ -17,16 +17,11 @@
             templateUrl: 'app/authorization/authorization.html',
             controller: 'authController',
             controllerAs : 'vm' //hierdoor moet scope nie geïnject worden
-<<<<<<< HEAD:StuckytoysFrontend/app/routes/routes.js
         }).when('/member',{
             url: '/member',
             templateUrl: 'app/members/memberOverview.html',
             controller: 'memberController',
-            controllerAs : 'vm' 
+            controllerAs : 'vm'
         }).otherwise({redirectTo: '/auth'});
-=======
-        }).otherwise({redirectTo:'/auth'}); //default pad
-
->>>>>>> e2889c8c78047a6565e0b9350eddd16065cee1d3:StuckytoysFrontend/app/authorization/authRoutes.js
     };
 })();
