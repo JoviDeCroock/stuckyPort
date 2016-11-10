233abcf81b2a336756a2c1a56c6c89ebabc39357
Joren Saey <joren.saey.t2795@student.hogent.be>
2016-11-10 12:35:02 +0100

    -- Basic storyController --
    controller

--- StuckytoysFrontend/app/stories/storyController.js
+++ StuckytoysFrontend/app/stories/storyController.js
@@ -11,7 +11,26 @@
     var vm = this;
     vm.username = authService.currentUser();
     //maak verhaal
-    
+    vm.selectedThemes = [];
+    vm.themes = [{
+      name: 'Recyclage',
+      description: 'Het kind leren omgaan met afval'
+    }, {
+      name: 'Pesten',
+      description: 'Moeilijk thema om over te praten'
+    }]; // test
+    vm.activeStory = {
+      name: '',
+      date: '',
+      theme: {},
+      scenes: []
+    };
+    vm.addTheme = addTheme;
+
+    function addTheme(){
+      
+    };
+
     //alle verhalen
     vm.logOut = logOut;
 
