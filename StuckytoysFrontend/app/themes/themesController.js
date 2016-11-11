8a4c2b0d82ee0772595b234bec12e7216f568e4c
evert.devos.v6255@student.hogent.be <evert.devos.v6255@student.hogent.be>
2016-11-11 12:57:29 +0100

    added themes controller and factory

---
+++ StuckytoysFrontend/app/themes/themesController.js
@@ -0,0 +1,46 @@
+(function() {
+    "use strict"
+
+    angular
+      .module('stuckyToys')
+      .controller('ThemesController', themesController);
+
+    themesController.$inject = ['$location', 'authService', 'themesFactory'];
+
+    function themesController($location, authService, themesFactory) {
+      var vm = this;
+
+      vm.themes = themesFactory.themes;
+      vm.getThemes = getAllThemes;
+      vm.addTheme = addTheme;
+      vm.editTheme = editTheme;
+      vm.logOut = logOut;
+
+      vm.username = authService.currentUser();
+
+      function getAllThemes() {
+        themesFactory.getAllThemes();
+      }
+
+      function addTheme(theme) {
+        themeFactory.addTheme(theme).error(function(error) {
+          vm.error = error;
+        }).succes(function(data) {
+          vm.themes.push(data);
+        });
+      }
+
+      function editTheme(theme) {
+        themeFactory.editTheme(theme)).error(function(error) {
+        vm.error = error;
+      }).succes(function(data) {
+        vm.themes.push(data);
+      });
+    }
+
+    function logOut(){
+      authService.logOut()
+      $location.path("/auth");
+    }
+  }
+})()
---
+++ StuckytoysFrontend/app/themes/themesFactory.js
@@ -0,0 +1,53 @@
+(function() {
+
+  'use strict';
+
+  angular
+    .module('stuckyToys')
+    .factory('themeFactory', themeFactory);
+
+  themeFactory.$inject = ['$http', 'url', 'authService'];
+
+  function themeFactory() {
+    var usedUrl = url.dev;
+    var token = authService.getToken();
+    var themeFactory = {
+      themes: [],
+      getAllThemes: getThemes,
+      addTheme: addTheme;
+    }
+
+    function getThemes() {
+      return $http.post(usedUrl + 'getAllThemes', {
+          headers: {
+            Authorization: 'Bearer ' + token
+          }).success(function() {
+          Angular.copy(data; themesFactory.themes);
+        });
+      }
+
+      function addTheme(theme) {
+        return $http.post(usedUrl + 'addtheme', theme, {
+          headers: {
+            Authorization: 'Bearer ' + token
+          }
+        }).success(function(data) {
+          themesFactory.themes.push(data);
+        });
+      }
+
+      function editTheme(theme) {
+        return $http.post(usedUrl + 'editTheme', theme, {
+          headers: {
+            Authorization: 'Bearer' + token
+          }
+        }).succes(function(data) {
+          themesFactory.themes.splice(themesFactory.themes.indexOf(theme),
+            1);
+          themesFactory.themes.push(data);
+        });
+      }
+    }
+
+  }
+})()
