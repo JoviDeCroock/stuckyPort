10a4dfef8404f146d02ea9be25236d2e217f4db6
Jovi De Croock <jovi.decroock.u8420@student.hogent.be>
2016-11-26 19:57:52 +0100

    -- CORS --
    Kleine change voor alle CORS ip's toe te staan

--- StuckytoysBackend/app.js
+++ StuckytoysBackend/app.js
@@ -48,7 +48,7 @@ app.set('view engine', 'ejs');
 //Middleware for allowing cross-origin api calls
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:8080');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     // Request headers you wish to allow
