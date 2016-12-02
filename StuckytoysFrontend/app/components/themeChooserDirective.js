530d3151804bec6be9d02a9abf0144b7a4896461
Joren Saey <joren.saey.t2795@student.hogent.be>
2016-12-02 12:08:49 +0100

    -- We komen er --

--- StuckytoysFrontend/app/components/themeChooserDirective.js
+++ StuckytoysFrontend/app/components/themeChooserDirective.js
@@ -9,10 +9,6 @@
       return {
         restrict: 'E',
         templateUrl: 'app/components/themeChooser.html',
-        controller: 'themesController',
-        controllerAs: 'vm',
-        scope: true,
-        bindToController: true,
         link: link
       };
       function link(scope, element, attrs, controller, transcludeFn) {
