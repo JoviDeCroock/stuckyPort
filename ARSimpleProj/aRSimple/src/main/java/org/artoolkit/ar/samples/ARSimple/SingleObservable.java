bfb0221f62b79a85cc274e54c205a560d06ce669
KEVINMSI\Kevin <kevin.schuddinck.v6468@student.hogent.be>
2016-12-16 10:37:42 +0100

    -- AR zip --

---
+++ ARSimpleProj/aRSimple/src/main/java/org/artoolkit/ar/samples/ARSimple/SingleObservable.java
@@ -0,0 +1,11 @@
+package org.artoolkit.ar.samples.ARSimple;
+
+/**
+ * Created by evert on 12/12/2016.
+ */
+
+public interface SingleObservable {
+
+    public void setObserver(SingleObserver obj);
+    public void updateObserver();
+}
