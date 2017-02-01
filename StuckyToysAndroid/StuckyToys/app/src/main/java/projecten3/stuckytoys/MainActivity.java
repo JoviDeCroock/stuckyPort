e5546f0837bfa2cf04a247b44c3d40403c47ee77
KEVINMSI\Kevin <kevin.schuddinck.v6468@student.hogent.be>
2017-02-01 23:44:13 +0100

    no message

--- StuckyToysAndroid/StuckyToys/app/src/main/java/projecten3/stuckytoys/MainActivity.java
+++ StuckyToysAndroid/StuckyToys/app/src/main/java/projecten3/stuckytoys/MainActivity.java
@@ -44,18 +44,25 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
-    @BindView(R.id.editEmail) EditText editEmail;
-    @BindView(R.id.editPassword) EditText editPassword;
-    @BindView(R.id.txtError) TextView txtError;
-    @BindView(R.id.btnLogin) Button btnLogin;
-    @BindView(R.id.checkboxStayLogged) CheckBox checkboxStayLogged;
+    @BindView(R.id.editEmail)
+    EditText editEmail;
+    @BindView(R.id.editPassword)
+    EditText editPassword;
+    @BindView(R.id.txtError)
+    TextView txtError;
+    @BindView(R.id.btnLogin)
+    Button btnLogin;
+    @BindView(R.id.checkboxStayLogged)
+    CheckBox checkboxStayLogged;
 
     private DomainController dc;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
@@ -70,7 +77,8 @@ public class MainActivity extends AppCompatActivity {
         boolean stayLogged = sharedPref.getBoolean("stayLogged", false);
 
         editEmail.setText(email);
-        if(stayLogged) {
+        if (stayLogged)
+        {
             checkboxStayLogged.setChecked(true);
             editPassword.setText(password);
             login(btnLogin);
@@ -78,7 +86,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @OnClick(R.id.btnLogin)
-    public void login(View view) {
+    public void login(View view)
+    {
         txtError.setText("");
         btnClickable(false);
 
@@ -86,28 +95,34 @@ public class MainActivity extends AppCompatActivity {
         final String password = editPassword.getText().toString();
 
         //alles ingevuld?
-        if( email.isEmpty() || password.isEmpty()) {
+        if (email.isEmpty() || password.isEmpty())
+        {
             txtError.setText(R.string.fill_all_fields);
             btnClickable(true);
             return;
         }
 
-        try {
+        try
+        {
             Call<User> call = dc.login(email, password);
 
-            call.enqueue(new Callback<User>() {
+            call.enqueue(new Callback<User>()
+            {
 
                 //Call to API using Retrofit; User as model; Body requires username & password (but username is actually email!);
                 //API returns a token; token is used to authenticate. also decoded token = id + exp + iat
                 //example token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ODE3ODU0YjE0NTRjNDFhODJlNmM3NzgiLCJleHAiOjE0ODMxOTc2NjYsImlhdCI6MTQ3ODAxMzY2Nn0.iDs223_K8SrtQlDHos5k1r8uRh8Pzq4-axjvZRPID4o
                 //decoded example token: {"_id":"5817854b1454c41a82e6c778","exp":1483197666,"iat":1478013666}
                 @Override
-                public void onResponse(Call<User> call, Response<User> response) {
-                    if (response.isSuccessful()) {
+                public void onResponse(Call<User> call, Response<User> response)
+                {
+                    if (response.isSuccessful())
+                    {
                         String token = response.body().getToken();
                         String tokenMiddle = token.split("\\.")[1];
                         String decoded = new String(Base64.decode(tokenMiddle, Base64.DEFAULT));
-                        try {
+                        try
+                        {
                             JSONObject jObj = new JSONObject(decoded);
                             //"exp" & "iat" also in this jsonobject
                             String id = jObj.getString("_id");
@@ -117,13 +132,17 @@ public class MainActivity extends AppCompatActivity {
                             Log.d("login", "id: " + id + " token: " + token);
 
                             getUserStories();
-                        } catch (JSONException ex) {
+                        } catch (JSONException ex)
+                        {
                             ex.printStackTrace();
                         }
-                    } else {
-                        if (response.code() == 401) {
+                    } else
+                    {
+                        if (response.code() == 401)
+                        {
                             txtError.setText(R.string.unauthorized);
-                        } else {
+                        } else
+                        {
                             txtError.setText(R.string.default_error);
                         }
                         Log.e("login", response.code() + " " + response.message());
@@ -132,27 +151,34 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 @Override
-                public void onFailure(Call<User> call, Throwable t) {
+                public void onFailure(Call<User> call, Throwable t)
+                {
                     noConnection();
                 }
             });
 
-        } catch (IOException ex) {
+        } catch (IOException ex)
+        {
             ex.printStackTrace();
             btnClickable(true);
         }
     }
 
-    private void getUserStories() {
+    private void getUserStories()
+    {
         Call<List<String>> call = dc.getUserStories();
 
-        call.enqueue(new Callback<List<String>>() {
+        call.enqueue(new Callback<List<String>>()
+        {
             @Override
-            public void onResponse(Call<List<String>> call, Response<List<String>> response) {
-                if (response.isSuccessful()) {
+            public void onResponse(Call<List<String>> call, Response<List<String>> response)
+            {
+                if (response.isSuccessful())
+                {
                     List<String> stories = response.body();
                     RealmList<RealmString> realmStories = new RealmList<RealmString>();
-                    for (String story : stories) {
+                    for (String story : stories)
+                    {
                         realmStories.add(new RealmString(story));
                     }
                     dc.getUser().setBoughtStories(realmStories);
@@ -161,11 +187,13 @@ public class MainActivity extends AppCompatActivity {
                     SharedPreferences sharedPref = getSharedPreferences(getString(R.string.preference_file_emailpassword), MODE_PRIVATE);
                     SharedPreferences.Editor editor = sharedPref.edit();
                     editor.putString("email", editEmail.getText().toString());
-                    if(checkboxStayLogged.isChecked()) {
+                    if (checkboxStayLogged.isChecked())
+                    {
                         editor.putString("password", editPassword.getText().toString());
                         editor.putBoolean("stayLogged", true);
                         editor.commit();
-                    } else {
+                    } else
+                    {
                         editor.putBoolean("stayLogged", false);
                         editor.commit();
                     }
@@ -175,10 +203,13 @@ public class MainActivity extends AppCompatActivity {
                     Intent intent = new Intent(MainActivity.this, StoryOverviewActivity.class);
                     startActivity(intent);
                     finish();
-                } else {
-                    if (response.code() == 401) {
+                } else
+                {
+                    if (response.code() == 401)
+                    {
                         txtError.setText(R.string.unauthorized);
-                    } else {
+                    } else
+                    {
                         txtError.setText(R.string.default_error);
                     }
                     Log.e("login", response.code() + " " + response.message());
@@ -187,26 +218,33 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onFailure(Call<List<String>> call, Throwable t) {
+            public void onFailure(Call<List<String>> call, Throwable t)
+            {
                 noConnection();
             }
         });
     }
 
-    private void noConnection() {
+    private void noConnection()
+    {
         txtError.setText(R.string.connection_error);
         final Context context = this;
 
         final Realm realm = Realm.getInstance(PersistenceController.CONFIG);
         final User user = realm.where(User.class).equalTo("email", editEmail.getText().toString()).equalTo("password", editPassword.getText().toString()).findFirstAsync();
-        user.addChangeListener(new RealmChangeListener<RealmModel>() {
+        user.addChangeListener(new RealmChangeListener<RealmModel>()
+        {
             @Override
-            public void onChange(RealmModel element) {
-                if (user.isValid()) {
+            public void onChange(RealmModel element)
+            {
+                if (user.isValid())
+                {
                     AlertDialog.Builder builder = new AlertDialog.Builder(context);
                     builder.setMessage(getString(R.string.continue_offline, user.getUsername()))
-                            .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int id) {
+                            .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener()
+                            {
+                                public void onClick(DialogInterface dialog, int id)
+                                {
                                     dc.setUser(realm.copyFromRealm(user));
                                     PersistenceController.internetConnection = false;
 
@@ -214,11 +252,13 @@ public class MainActivity extends AppCompatActivity {
                                     SharedPreferences sharedPref = getSharedPreferences(getString(R.string.preference_file_emailpassword), MODE_PRIVATE);
                                     SharedPreferences.Editor editor = sharedPref.edit();
                                     editor.putString("email", editEmail.getText().toString());
-                                    if(checkboxStayLogged.isChecked()) {
+                                    if (checkboxStayLogged.isChecked())
+                                    {
                                         editor.putString("password", editPassword.getText().toString());
                                         editor.putBoolean("stayLogged", true);
                                         editor.commit();
-                                    } else {
+                                    } else
+                                    {
                                         editor.putBoolean("stayLogged", false);
                                         editor.commit();
                                     }
@@ -228,14 +268,17 @@ public class MainActivity extends AppCompatActivity {
                                     finish();
                                 }
                             })
-                            .setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int id) {
+                            .setNegativeButton(R.string.no, new DialogInterface.OnClickListener()
+                            {
+                                public void onClick(DialogInterface dialog, int id)
+                                {
                                     dialog.cancel();
                                     btnClickable(true);
                                 }
                             });
                     builder.show();
-                } else {
+                } else
+                {
                     txtError.setText(R.string.connection_error_user_unknown);
                     btnClickable(true);
                 }
@@ -243,34 +286,43 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void btnClickable(boolean clickable) {
+    private void btnClickable(boolean clickable)
+    {
         //background green if clickable; gray if not clickable
         //using setBackgroundDrawable because setBackground doesn't work on API 15
         btnLogin.setClickable(clickable);
-        if (clickable) {
+        if (clickable)
+        {
             btnLogin.setBackgroundDrawable(ContextCompat.getDrawable(this, R.color.clickable));
-        } else {
+        } else
+        {
             btnLogin.setBackgroundDrawable(ContextCompat.getDrawable(this, R.color.unclickable));
         }
     }
 
     @OnClick(R.id.txtNotRegistered)
-    public void register() {
+    public void register()
+    {
         Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
         startActivity(intent);
     }
 
     @Override
-    public void onBackPressed() {
+    public void onBackPressed()
+    {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage(R.string.exit)
-                .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
+                .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener()
+                {
+                    public void onClick(DialogInterface dialog, int id)
+                    {
                         System.exit(0);
                     }
                 })
-                .setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
+                .setNegativeButton(R.string.no, new DialogInterface.OnClickListener()
+                {
+                    public void onClick(DialogInterface dialog, int id)
+                    {
                         dialog.cancel();
                     }
                 });
